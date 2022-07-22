0b7d2990665a50ea6b5d966f596fd97a05411a88
ifti01 <iftekhar.fahim97@gmail.com>
2022-07-22 17:51:04 +0600

    Add project files.

---
+++ SignIn/Controllers/LoginController.cs
@@ -0,0 +1,71 @@
+﻿using SignIn.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace SignIn.Controllers
+{
+    public class LoginController : Controller
+    {
+        SignInLoginEntities db = new SignInLoginEntities();
+
+        // GET: Login
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult Index(User userobj)
+        {
+            var user = db.Users.Where(model => model.UserName == userobj.UserName &&  model.Password == userobj.Password).FirstOrDefault();
+            if(user != null)
+            {
+                Session["UserId"] = userobj.Id.ToString();
+                Session["UserName"] = userobj.UserName.ToString();
+                TempData["LoginSuccessMessage"] = "<script>alert('SAved Successfully'')</script>";
+                return RedirectToAction("Index","User");
+            }
+
+            else
+            {
+                ViewBag.ErrorMessage = "<script>alert('Username or Password incorrect')</script>";
+            }
+
+            return View();
+        }
+
+        public ActionResult Signup()
+        {
+            return View();
+        }
+
+        [HttpPost]
+
+        //User - Model,Users -- Table//
+        public ActionResult Signup(User userobject)
+        {
+            if (ModelState.IsValid == true)
+            {
+                db.Users.Add(userobject);
+                int a =   db.SaveChanges();
+
+                if (a > 0)
+                {
+                    ViewBag.InsertMessage = "<script>alert('Registerd Successfully'')</script>";
+                    ModelState.Clear();
+                }
+
+                else
+                {
+                    ViewBag.InsertMessage = "<script>alert('Registerd Failed'')</script>";
+                }
+            }
+
+            return View();
+     }
+   
+    }
+}
---
+++ SignIn/Controllers/UserController.cs
@@ -0,0 +1,76 @@
+﻿using SignIn.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace SignIn.Controllers
+{
+    public class UserController : Controller
+    {
+        SignInLoginEntities db = new SignInLoginEntities();
+
+        // GET: User
+        public ActionResult Index()
+        {
+
+            if (Session["UserId"] == null)
+            {
+                return RedirectToAction("Index", "Login");
+            }
+
+            else {
+                //var model = new UserTask();
+                //model.userTasks= db.UserTasks.ToList();
+                //return View(model);
+                List<UserTask> userTasks= db.UserTasks.ToList();
+                return View(userTasks);
+            }
+            
+        }
+
+
+        public ActionResult Task() {
+
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult Task(UserTask userTaskobj) {
+
+            
+            {
+                if (ModelState.IsValid == true)
+                {
+                    db.UserTasks.Add(userTaskobj);
+                    int a = db.SaveChanges();
+
+                    if (a > 0)
+                    {
+                        ViewBag.InsertMessage = "<script>alert('Registerd Successfully'')</script>";
+                        ModelState.Clear();
+                        return View();
+                    }
+
+                    else
+                    {
+                        ViewBag.InsertMessage = "<script>alert('Registerd Failed'')</script>";
+                    }
+                }
+
+            }
+
+
+
+
+            return View();
+        }
+
+        public ActionResult Logout() {
+            Session.Abandon();
+            return RedirectToAction("Index","Login");
+        }
+
+    }
+}
